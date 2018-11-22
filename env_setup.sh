093909cfb2ac1d6fdaaefd5c8c38ba7e33a2ae57
Jyri Ilama <jyri.ilama@siili.com>
2018-11-22 09:47:09 +0200

    also parametrize env_setup to install correct webdrivermanager

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+if [[ $# -eq 0 ]] ; then
+    export browser = chrome
+else
+    export browser = $1
+fi
+
 python3 -m venv ./venv
 . ./venv/bin/activate
 pip install --upgrade pip
 pip install -r ./requirements.txt
-webdrivermanager chrome
+webdrivermanager $browser
